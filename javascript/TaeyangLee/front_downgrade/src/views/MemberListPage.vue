7a776a82f60ba8f8407900e305899133004aab21
rudghxodid <rudghxodid@naver.com>
2021-07-16 15:02:53 +0900

    7월15일 복습+퀴즈도전(실패,,)

---
+++ javascript/TaeyangLee/front_downgrade/src/views/MemberListPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id='member'>
+        <h2>회원가입</h2>
+        <router-link :to="{ name: 'MemberRegisterPage' }">
+            회원가입합시다
+        </router-link>
+        <!--<board-list/> -->
+    </div>
+</template>
+
+<script>
+//import { mapState, mapActions } from 'vuex'
+
+export default {
+    name: 'MemberRegisterPage',
+    components: {
+
+    },
+    computed: {
+     
+    }
+}
+</script>
---
+++ javascript/TaeyangLee/front_downgrade/src/views/MemberRegisterPage.vue
@@ -0,0 +1,38 @@
+<template>
+    <div align="center">
+        <h2>회원</h2>
+        <member-register-form @submit="onMemberSubmit"/>
+    </div>
+</template>
+
+
+<script>
+import MemberRegisterForm from '@/components/member/MemberRegisterForm'
+import axios from 'axios'
+
+
+export default {
+    name: 'MemberRegisterPage',
+    components: {
+        MemberRegisterForm
+    },
+    methods: {
+        onMemberSubmit (payload) {
+            const { id, pw } = payload
+            axios.post('http://localhost:7777/VueMember/register',{ id, pw })
+            .then(res => {
+                alert('등록 성공!' + res)
+               /* this.$router.push({
+                    name: 'BoardReadPage',
+                    params: {boardNo: res.data.boardNo.toString() }
+                })
+                */
+            })
+            .catch(res => {
+                alert(res.response.data.message)
+            })
+        }
+    }
+
+}
+</script>
